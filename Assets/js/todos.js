38a10461c6b80eeb77f2c63b1c74d03003546438
steverogers07 <dvas325@gmail.com>
2020-03-17 04:21:24 +0530

    Something New

---
+++ Assets/js/todos.js
@@ -0,0 +1,44 @@
+// Check off Specific Todos By Clicking
+$("ul").on("click", "li" , function(){
+	// //if li is gray
+	// if($(this).css("color")==="rgb(128,128,128)") {// Always use rgb in conditional
+	// 	$(this).css({
+	// 		color: "black",
+	// 		textDecoration: "none"
+	// 	});
+	// 	//turn it black
+	// }
+	// //else 
+	// else {
+	// 		$(this).css({
+	// 		color: "gray",
+	// 		textDecoration: "line-through"
+	// 	});
+	// 	//turn it gray
+	// }
+	               //   OR   //
+	
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function() {
+		$(this).remove();
+	});
+	event.stopPropagation();  // jQuery Method
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which===13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function() {
+	("input[type='text']").fadeToggle();
+});
